6d8ea437babff403a6029700f6bd46ff62a04071
June <juneyoon@users.noreply.github.com>
2019-08-30 00:03:54 -0600

    Update PythonFlaskApi.py

--- PythonFlaskApi.py
+++ PythonFlaskApi.py
@@ -27,10 +27,35 @@ def newMenuItem(cooking_id):
         newItem = MenuItem(name = request.form['name'], description = request.form['description'], cooking_id = cooking_id)
         session.add(newItem)
         session.commit()
-        return redirect(url_for('menu.html', cooking_id = cooking_id))
+        items = session.query(MenuItem).filter_by(cooking_id = cooking_id).all()
+        return render_template('menu.html', items=items, cooking_id=cooking_id)
     else:
-      return render_template('newmenuitem.html', cooking_id = cooking_id)    
+      return render_template('newmenuitem.html', cooking_id=cooking_id)    
 
+@app.route('/menu/<int:cooking_id>/new', methods=['GET','POST'])
+def editMenuItem(cooking_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], description = request.form['description'], cooking_id = cooking_id)
+        session.add(newItem)
+        session.commit()
+        items = session.query(MenuItem).filter_by(cooking_id = cooking_id).all()
+        return render_template('menu.html', items=items, cooking_id=cooking_id)
+    else:
+      return render_template('editmenuitem.html', cooking_id=cooking_id)    
+
+@app.route('/menu/<int:cooking_id>/<int:menu_id>/delete', methods=['GET','POST'])
+def deleteMenuItem(cooking_id, menu_id):
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        cooking = session.query(Cooking).all()
+        return render_template('index.html', cooking=cooking)
+    else:
+        items = session.query(MenuItem).filter_by(cooking_id = cooking_id).all()
+        return render_template('menu.html', items=items, cooking_id=cooking_id)         
+  
+  
 if __name__ == '__main__':
     app.debug=True
     app.run(host='0.0.0.0')
